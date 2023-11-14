586eae35bbb25c00b6569a6b205f7e1d2e4ccf5b
AlgoEstranho13 <rafael2001bsantos@gmail.com>
2023-11-14 00:31:48 -0300

    Primeiro Commit

---
+++ src/view/Principal.java
@@ -0,0 +1,26 @@
+package view;
+
+import br.edu.fateczl.arvore.caractere.ArvoreChar;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		ArvoreChar a = new ArvoreChar();
+		char[] vetor = {'k', 'd', 'm', 'b', 'f', 'l', 't', 'c', 'p', 'z', 'r'};
+		for (char i: vetor) {
+			a.insert(i);
+		}
+		try {
+			a.postfixSearch();
+			a.remove('m');
+			a.prefixSearch();
+			a.infixSearch();
+			a.postfixSearch();
+			a.search('r');
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
